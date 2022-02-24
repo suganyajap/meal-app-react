386b2a5af34db4208f872bb5d0858898e4096a2d
SUNDARAVADIVU R <suganyajap@gmail.com>
2022-02-24 08:58:21 +0530

    url changed

--- src/Axios.js
+++ src/Axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-axios.defaults.baseURL="http://localhost:9000";
+axios.defaults.baseURL="https://my-meals-app.herokuapp.com";
 axios.interceptors.request.use((config)=>{
 const token=localStorage.getItem('token');
 const auth=token ? `Bearer ${token}` : "";
